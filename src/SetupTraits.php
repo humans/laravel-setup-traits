c5a56ecfb3f65b11cd3955562af50e312d87227a
Jaggy <jaggy@hey.com>
2021-04-10 12:21:31 +0800

    Add support for tearDown.

--- src/SetupTraits.php
+++ src/SetupTraits.php
@@ -23,4 +23,28 @@ trait SetupTraits
             call_user_func([$this, $method]);
         }
     }
+
+
+    protected function tearDown(): void
+    {
+        $this->tearDownAdditionalTraits();
+
+        parent::tearDown();
+    }
+
+    public function tearDownAdditionalTraits()
+    {
+        $uses = array_flip(class_uses_recursive(static::class));
+
+
+        foreach($uses as $trait) {
+            $method = 'tearDown' . class_basename($trait);
+
+            if (! method_exists($this, $method)) {
+                continue;
+            }
+
+            call_user_func([$this, $method]);
+        }
+    }
 }
